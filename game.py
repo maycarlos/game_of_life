b537122032147de8141be7e55a688e52c5704d85
Elmer <56842806+maycarlos@users.noreply.github.com>
2021-07-26 22:19:29 +0100

    Erro:Calculate the next state of the board

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import numpy as np
-ALIVE = 1.0
-DEAD = 0.0
+ALIVE = 1
+DEAD = 0
 
-def dead_state(width, height):
+def dead_state(width, height) -> np.ndarray:
 
-    return np.zeros((width, height))
+    return np.zeros((width, height), dtype=np.int0)
 
-def random_state(width, height):
+def random_state(width, height) -> np.ndarray:
 
     state = dead_state(width, height)
 
@@ -21,7 +21,64 @@ def random_state(width, height):
 
     return state
 
-def render(board_state):
+def next_board_state(board_state) -> np.ndarray:
+    """
+    Regras:
+        1- Uma célula viva com 0 ou 1 vizinho morre.  
+        2- Uma célula viva com 2 ou 3 vizinhos continua viva.  
+        3- Uma célula viva com mais do que 3 vizinhos morre.  
+        4- Uma célula morta com 3 vizinhos vivos torna-se viva.  
+    """
+    # TODO fazer a soma dos valores do vizinhos e alterar o estado da celula consoante o resultado
+    # se for -∞, 1]  celula = DEAD
+    # se for  [2, 3] pass
+    # se for  ]3, +∞ celula = dead
+    # se o celulad DEAD e com soma = 3 celula = ALIVE
+
+    next_state = dead_state(*board_state.shape)
+
+    for x in range(len(board_state)):
+        for y in range(len(board_state[0])):
+
+            if y == 0:
+                if x == 0:
+                    status = np.sum([board_state[x,y+1],board_state[x+1,y],board_state[x+1,y+1]])
+
+                elif x == board_state.shape[0]-1:
+                    status = np.sum([board_state[x-1,y],board_state[x,y+1],board_state[x-1,y+1]])
+
+                else:
+                    status = np.sum([board_state[x-1,y],board_state[x+1,y],board_state[x-1,y+1],board_state[x,y+1]], board_state[x+1,y+1])
+
+            elif y == board_state.shape[1]-1:
+                if x == 0:
+                    status = np.sum([board_state[x,y-1],board_state[x+1,y],board_state[x+1,y-1]])
+
+                elif x == board_state.shape[0]:
+                    status = np.sum([board_state[x-1,y],board_state[x-1,y-1],board_state[x,y-1]])
+
+                else:
+                    status = np.sum([board_state[x-1,y],board_state[x+1,y],board_state[x-1,y-1],board_state[x,y-1]], board_state[x+1,y-1])
+            else:
+                status = np.sum([board_state[x-1,y-1],board_state[x,y-1],board_state[x+1,y-1],
+                    board_state[x-1, y],board_state[x+1,y],
+                    board_state[x-1,y+1],board_state[x,y+1],board_state[x+1,y+1]])
+
+            if board_state[x,y] == ALIVE and status <= 1:
+                next_state[x,y] = DEAD #UNDER_POP
+
+            elif board_state[x,y] == ALIVE and 2 <= status <= 3:
+                pass
+
+            elif board_state[x,y] == ALIVE and status > 3:
+                next_state[x,y] = DEAD #OVER_POP
+
+            elif board_state[x,y] == DEAD and status == 3:
+                next_state[x,y] = ALIVE
+
+    return next_state
+
+def render(board_state) -> None:
 
     render_state = board_state.tolist()
 
@@ -35,12 +92,11 @@ def render(board_state):
     print("\n".join(renderized))
 
 def main():
+    coisa = random_state(3,3)
 
-    render(random_state(5,5))
+    render(coisa)
     print("\n")
-    render(random_state(10,10))
-    print("\n")
-    render(random_state(20,20))
+    render(next_board_state(coisa))
 
 if __name__ == "__main__":
     main()
